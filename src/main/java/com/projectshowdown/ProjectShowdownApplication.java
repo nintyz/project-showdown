fca001a2a7967e389cc01d34c65aadc8b2841c4c
Aaron <aaronjiteck@gmail.com>
2024-09-18 17:34:24 +0800

    initial commit

---
+++ src/main/java/com/projectshowdown/ProjectShowdownApplication.java
@@ -0,0 +1,13 @@
+package com.projectshowdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectShowdownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectShowdownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projectshowdown/ProjectShowdownApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectshowdown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectShowdownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
