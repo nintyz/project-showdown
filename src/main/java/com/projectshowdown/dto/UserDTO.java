5dfdede3a87346324638ee9f63c5542e0d4175a1
Aaron <aaronjiteck@gmail.com>
2024-10-12 00:29:42 +0800

    code cleanup to use lombok annotations

--- src/main/java/com/projectshowdown/dto/UserDTO.java
+++ src/main/java/com/projectshowdown/dto/UserDTO.java
@@ -1,8 +1,16 @@
 package com.projectshowdown.dto;
 
 import com.projectshowdown.entities.Player;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 //special User object to ensure firebase only store the variables here.
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
     private String id;
     private String email;
@@ -10,61 +18,5 @@ public class UserDTO {
     private String role;
     private Player playerDetails;
 
-    public UserDTO(String id, String email, String password, String role, Player playerDetails) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.role = role;
-        this.playerDetails = playerDetails;
-    }
 
-    public UserDTO() {
-    }
-
-    // Getters and Setters
-    
-    
-    public String getEmail() {
-        return email;
-    }
-
-    public UserDTO(String id) {
-        this.id = id;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public Player getPlayerDetails() {
-        return playerDetails;
-    }
-
-    public void setPlayerDetails(Player playerDetails) {
-        this.playerDetails = playerDetails;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 }
