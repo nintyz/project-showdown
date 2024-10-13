8e123583a89fa77e3abfff8d36dff48284eb101d
Aaron <aaronjiteck@gmail.com>
2024-10-14 00:08:49 +0800

    encoded password when adding user

--- src/main/java/com/projectshowdown/controllers/UserController.java
+++ src/main/java/com/projectshowdown/controllers/UserController.java
@@ -1,15 +1,9 @@
 package com.projectshowdown.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.projectshowdown.dto.UserDTO;
 import com.projectshowdown.entities.User;
@@ -26,6 +20,9 @@ import java.util.List;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     public UserController(UserService userService) {
         this.userService = userService;
@@ -54,6 +51,10 @@ public class UserController {
     @PostMapping("/users")
     public String addPlayer(@Valid @RequestBody User playerData) throws ExecutionException, InterruptedException {
         // return playerData.getPlayerDetails().getName().toString();
+
+        // Encode the password before storing it
+        playerData.setPassword(passwordEncoder.encode(playerData.getPassword()));
+
         return userService.addPlayer(playerData);
     }
 
