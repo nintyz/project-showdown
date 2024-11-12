8d4f65824af9306a5acb4aa9b0030ba49bb341c4
Coben Yap <cobenyapsh@gmail.com>
2024-11-12 12:53:38 +0800

    Fixed some duplicate method bug

--- frontend/src/views/VerifyAccount.vue
+++ frontend/src/views/VerifyAccount.vue
@@ -202,12 +202,7 @@ export default {
         this.$router.push('/dashboard');
       }
     },
-    numberOnly(event) {
-      const charCode = event.which ? event.which : event.keyCode;
-      if (charCode > 31 && (charCode < 48 || charCode > 57)) {
-        event.preventDefault();
-      }
-    }
+    
   }
 };
 </script>
