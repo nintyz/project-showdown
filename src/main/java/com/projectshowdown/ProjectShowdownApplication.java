49647efb1aa3527252d96f92b15d0f655bafe70d
Coben Yap <cobenyapsh@gmail.com>
2024-11-13 10:10:19 +0800

    Fixed broken main

--- src/main/java/com/projectshowdown/ProjectShowdownApplication.java
+++ src/main/java/com/projectshowdown/ProjectShowdownApplication.java
@@ -4,15 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 @EnableScheduling // This enables scheduling throughout the application
 public class ProjectShowdownApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load(); // This loads variables from the .env file
-        System.setProperty("GOOGLE_CONFIG_PATH", dotenv.get("GOOGLE_CONFIG_PATH"));
         SpringApplication.run(ProjectShowdownApplication.class, args);
     }
 
